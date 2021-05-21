628007d6b78110af7b229023f861eb62adcd6345
Andiappan <ARandi@outlook.com>
2021-05-21 12:32:22 +0400

    main:Repository Initiation

---
+++ Src/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Law
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Src/Controllers/LawController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.Law.Controllers
+{
+    public class LawController : Controller
+    {
+        // GET: Law
+        public ActionResult Setup()
+        {
+            return View();
+        }
+    }
+}
